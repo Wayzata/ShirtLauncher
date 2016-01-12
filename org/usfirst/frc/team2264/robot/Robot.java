7a413d98a89350e40c5fe9e20feb3b50295854f0
Nathan Ringo <remexre@gmail.com>
2016-01-12 07:45:52 -0600

    Adds more commenting.

--- org/usfirst/frc/team2264/robot/Robot.java
+++ org/usfirst/frc/team2264/robot/Robot.java
@@ -12,28 +12,59 @@ import xyz.remexre.robotics.util.Monostable;
  * @author Nathan Ringo
  */
 public class Robot extends IterativeRobot {
-	private RobotDrive drive;
-	private Solenoid solenoid;
-	private Joystick joystick;
-	private Monostable fireButton;
-	
-	/**
-	 * The robotInit method sets up the programming of the robot.
-	 */
+    // The RobotDrive class manages multiple motors. This means that we can, for
+    // example, specify "move at 80% power at a 35 degree angle" instead of
+    // "set left motor to 92.5% power and right motor to 77.5% power".
+    private RobotDrive drive;
+
+    // A solenoid is used to trigger hydraulics.
+    private Solenoid solenoid;
+
+    // A Joystick is used to manage input from one Joystick, XBox Controller, or
+    // (hopefully) Steam Controller. /r/FRC says Steam Controllers work, at
+    // least, and shiny.
+    private Joystick joystick;
+
+    // The Monostable class is a custom class for use with input buttons -- to
+    // avoid triggering the same action repeatedly when a button is held down,
+    // it only returns that an event has occured at the first instant that it
+    // has.
+    private Monostable fireButton;
+
+    /**
+     * The robotInit method sets up the robot's hardware. Almost all the work
+     * here should be initializing variables with references to hardware.
+     */
     public void robotInit() {
-    	Talon leftMotor = new Talon(0),
-    			rightMotor = new Talon(1);
-    	this.drive = new RobotDrive(leftMotor, rightMotor);
-    	this.solenoid = new Solenoid(2);
-    	this.joystick = new Joystick(0);
-    	this.fireButton = new Monostable(this.joystick, 1);
+        // Talons are the motor controllers we use. Basically, they convert
+        // something like "25% power" to something like "9.8 volts".
+        Talon leftMotor = new Talon(0),
+            rightMotor = new Talon(1);
+        // The RobotDrive class takes the motors that it will manage as inputs.
+        this.drive = new RobotDrive(leftMotor, rightMotor);
+
+        // I'm assuming we're connecting the solenoid over PWM port 2, as ports
+        // 0 and 1 go to the motors.
+        this.solenoid = new Solenoid(2);
+
+        // The first joystick is joystick zero...
+        this.joystick = new Joystick(0);
+
+        // ...but the first button is button one.
+        this.fireButton = new Monostable(this.joystick, 1);
     }
 
     /**
      * The teleopPeriodic method is called repeatedly during the teleop phase.
      */
     public void teleopPeriodic() {
+        // This basically says "read the position of the joystick, then use that
+        // to configure the motor speeds".
         this.drive.arcadeDrive(this.joystick);
+
+        // This will instantaneously trigger the solenoid when the fire button
+        // is pressed. I'm not sure how long we need to hold the solenoid open
+        // for, so for now it'll trigger only for a short burst.
         this.solenoid.set(this.fireButton.getAsBoolean());
     }
 }
--- xyz/remexre/robotics/util/Monostable.java
+++ xyz/remexre/robotics/util/Monostable.java
@@ -5,32 +5,54 @@ import java.util.function.BooleanSupplier;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Trigger;
 
+/**
+ * The Monostable class takes an input and will return true itself the first
+ * time its state is checked after the underlying function is true, then false
+ * until the next transition from false to true by the underlying function.
+ * @author Nathan Ringo
+ */
 public class Monostable implements BooleanSupplier {
 	private BooleanSupplier triggerFn;
 	private boolean last;
 
+	/**
+	 * Bases a Monostable off of an arbitrary boolean function.
+	 */
 	public Monostable(BooleanSupplier triggerFn) {
 		this.triggerFn = triggerFn;
 		this.last = false;
 	}
+	/**
+	 * Bases a Monostable off of a particular button on a joystick.
+	 */
 	public Monostable(Joystick joystick, int button) {
 		this.triggerFn = () -> joystick.getRawButton(button);
 		this.last = false;
 	}
+	/**
+	 * Bases a Monostable off of a Trigger object.
+	 */
 	public Monostable(Trigger trigger) {
 		this.triggerFn = trigger::get;
 		this.last = false;
 	}
-	
+
+	/**
+	 * Returns true if a state transition from false to true has occured on the
+	 * underlying function since the last time this function was called.
+	 */
 	@Override
 	public boolean getAsBoolean() {
-		if(this.triggerFn.getAsBoolean()) {
-			if(this.last) {
-				return false;
-			} else {
+		if(this.triggerFn.getAsBoolean()) { // If the trigger is true now...
+			if(this.last) { // But was true before...
+				return false; // No transition occured.
+			} else { // And was false before...
+				// A transition occured, so update the internal state to account
+				// for this and return true.
 				return (this.last = true);
 			}
-		} else {
+		} else { // Otherwise...
+			// Set the internal state to false and return false.
 			return (this.last = false);
 		}
 	}
